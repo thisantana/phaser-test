a8c005aba1d3b17ca50420a5dc9a37d02efa1763
thisantana <thiago.fdsantana@gmail.com>
2017-08-30 20:55:48 -0300

    Add sprites , changeState Listeners and movement keys

--- js/state0.js
+++ js/state0.js
@@ -1,14 +1,44 @@
 var demo = {};
+var centerX= 1500/2;
+var centerY= 1000/2;
+var aircraft;
+var defaultSpeed = 15;
+
+
 demo.state0 = function(){}
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+
+        //Load Default Images
+        game.load.image('starfield','./assets/sprites/space.png');
+        game.load.image('aircraft','./assets/sprites/Aircraft2.png');
+    },
     create: function(){
-        game.stage.backgroundColor = "#000";
+        game.stage.backgroundColor = "#0f2";
         console.log('state0');
         changeStateListeners();
-        
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.add.sprite(0,0,'starfield');
+        aircraft = game.add.sprite(centerX,centerY,'aircraft');
+        aircraft.anchor.setTo(0.5,0.5);
     },
-    update: function(){}
+    update: function(){
+        var moveRight = game.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+        var moveLeft = game.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+        var moveUp = game.input.keyboard.isDown(Phaser.Keyboard.UP);
+        var moveDown = game.input.keyboard.isDown(Phaser.Keyboard.DOWN);
+
+        //Movement Keys
+        if(moveRight){
+            aircraft.x += defaultSpeed;
+        }else if(moveLeft) {
+            aircraft.x += -defaultSpeed
+        }else if(moveUp) {
+            aircraft.y += -defaultSpeed
+        }else if(moveDown) {
+            aircraft.y += defaultSpeed
+        }
+    }
 };
 
 function changeState(i,stateNum) {
