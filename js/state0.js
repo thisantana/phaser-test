885f420c1f9556498c4f9dc42f4c9dd4c1e381b6
thisantana <thiago.fdsantana@gmail.com>
2017-08-30 16:07:01 -0300

    Add changeState Key

--- js/state0.js
+++ js/state0.js
@@ -1,11 +1,17 @@
-demo.state1 = function(){}
-demo.state1.prototype = {
+var demo = {};
+demo.state0 = function(){}
+demo.state0.prototype = {
     preload: function(){},
     create: function(){
-        game.stage.backgroundColor = "#00f";
-        console.log('state1');
+        game.stage.backgroundColor = "#000";
+        console.log('state0');
         game.input.keyboard.addKey(Phaser.Keyboard.ONE).onDown.add(changeState, null, null, 1);
         game.input.keyboard.addKey(Phaser.Keyboard.ZERO).onDown.add(changeState, null, null, 0);
     },
     update: function(){}
 };
+
+function changeState(i,stateNum) {
+    game.state.start('state'+stateNum);
+    console.log(stateNum);
+}
